e46f0a29d592611b94e6030364ad7e2ec12fb954
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-12-14 17:11:00 +0100

    chnage the code to connect withe database

--- app/configs/database.php
+++ app/configs/database.php
@@ -4,16 +4,10 @@ require('config.php');
 
 
 try {
-$conn = new PDO("mysql:host=". HOST, USER , PASSWORD);
+$conn = new PDO("mysql:host=". HOST .";dbname=" . DB , USER , PASSWORD);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$sql="CREATE DATABASE IF NOT EXISTS `". DB ."`";
-$conn->exec($sql);
-$conn->exec("USE `". DB ."`");
-echo "Database created or already exists. <br/>";
+echo "Database connected seccessfully. <br/>";
 }catch (PDOException $e) {
     echo $sql . "<br/>" . $e->getMessage();
 }
-
-$conn = null;
-
 ?>
