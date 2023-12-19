c038e254203b4efa0fa3f6f5a5b070acd8eeed3f
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-12-19 20:57:34 +0100

    create a folder reposotories for database

---
+++ app/repositories/Database.php
@@ -0,0 +1,24 @@
+<?php
+
+require("../config/config.php");
+
+class Database{
+
+protected $db;
+
+public function connect(){
+
+    try {
+        $db =new PDO("mysql:host=" .HOST.";dbname=" . DB ,USER , PASSWORD);
+        return $db; 
+    } catch (PDOException $e) {
+        $e->getMessage();
+    }
+
+}
+
+}
+
+
+
+?>
